3b4245f0c7b0b6671fb655ebc8ee3e4a6292e36d
alewin <ko.alessio@gmail.com>
2016-07-04 17:43:10 +0200

    dynamic get files

--- get_dir_files.c
+++ get_dir_files.c
@@ -4,6 +4,24 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+// usage   char **files = NULL; int n = 0; get_c_files(pathname, &files, n);
+void dynamic_get_files(char *pathname, char ***cfiles, int i) {
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir(pathname)) != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      if (ent->d_type == DT_REG) {
+        *cfiles = realloc(*cfiles, (i + 1) * sizeof(char *));
+        (*cfiles)[i] = malloc((strlen(ent->d_name)) + 1);
+        strcpy((*cfiles)[i], ent->d_name);
+        i++;
+      }
+    }
+    printf("files[%d] = %s \n", 0, (*cfiles)[0]);
+    closedir(dir);
+  }
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Expected 1 parameter");
