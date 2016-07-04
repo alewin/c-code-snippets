17ef81583f6fced211ba77cbb91bee8d111d1535
alewin <ko.alessio@gmail.com>
2016-07-04 16:03:27 +0200

    get file extension example

---
+++ get_file_extension.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+const char *get_filename_ext(const char *filename) {
+  const char *dot = strrchr(filename, '.');
+  if (!dot || dot == filename)
+    return "";
+  return dot + 1;
+}
+
+int main(int argc, char *argv[]) {
+  printf("%s\n", get_filename_ext("test.tiff"));
+  printf("%s\n", get_filename_ext("test.blah.tiff"));
+  printf("%s\n", get_filename_ext("test."));
+  printf("%s\n", get_filename_ext("test"));
+  printf("%s\n", get_filename_ext("..."));
+  return 0;
+}
